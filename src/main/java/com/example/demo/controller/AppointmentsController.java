f00e7f60f3909021e0567a5fdf9950cfc5e211cc
aleksejmoksancev <mokshantsev.aleksei>
2023-05-09 16:52:39 +0500

    add bin file

--- src/main/java/com/example/demo/controller/AppointmentsController.java
+++ src/main/java/com/example/demo/controller/AppointmentsController.java
@@ -7,6 +7,7 @@ import com.example.demo.services.AppointmentService;
 import com.example.demo.services.PatientService;
 import com.example.demo.utils.Constraints;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
